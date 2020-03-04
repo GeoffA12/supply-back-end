027c210632d7d7b7ccdc5cc624dd6caccbbbf880
komoto415 <jeffreyng99@gmail.com>
2020-03-04 05:53:58 -0600

    added version num

--- ENUMS/servicetype.py
+++ ENUMS/servicetype.py
@@ -1,6 +1,8 @@
 from enum import Enum
 
 class type(Enum):
+    ver = '0.1'
+
     DRYCLEANING = 'drycleaning'
     RX = 'rx'
     COFFEE = 'coffee'
--- ENUMS/vehiclestatus.py
+++ ENUMS/vehiclestatus.py
@@ -1,6 +1,8 @@
 from enum import Enum
 
 class status(Enum):
+    ver = '0.1'
+
     ACTIVE = 'active'
     INACTIVE = 'inactive'
     MAINTENANCE = 'maintenance'
