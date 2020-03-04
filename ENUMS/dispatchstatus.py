2bee49e05e463439cc621da7770c6eb9306a0f21
komoto415 <jeffreyng99@gmail.com>
2020-03-04 05:42:42 -0600

    dispatchstatus enum and unittest for enum

---
+++ ENUMS/dispatchstatus.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class status(Enum):
+    QUEUED = 'queued'
+    RUNNING = 'running'
+    DONE = 'done'
---
+++ UNITTEST/TestDispatchStatus.py
@@ -0,0 +1,27 @@
+import unittest
+import sys
+
+sys.path.insert(1, '../ENUMS/')
+
+from dispatchstatus import status
+
+class MyTestCase(unittest.TestCase):
+
+    def test_queued(self):
+        self.assertEqual('queued', status.QUEUED.value)
+        self.assertTrue(isinstance(status.QUEUED, status))
+        print(status.QUEUED.name)
+
+    def test_running(self):
+        self.assertEqual('running', status.RUNNING.value)
+        self.assertTrue(isinstance(status.RUNNING, status))
+        print(status.RUNNING.name)
+
+    def test_maintenance(self):
+        self.assertEqual('done', status.DONE.value)
+        self.assertTrue(isinstance(status.DONE, status))
+        print(status.DONE.name)
+
+
+if __name__ == '__main__':
+    unittest.main()
