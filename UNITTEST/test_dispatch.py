7bff904c9d8dc236d87b726a6a95ff2a89dd6cb5
Jeffrey Ng <komoto415@gmail.com>
2020-03-12 11:19:23 -0500

    better class names, better formatting

--- UNITTEST/test_dispatch.py
+++ UNITTEST/test_dispatch.py
@@ -4,7 +4,7 @@ import sys
 
 sys.path.insert(1, '../')
 from dispatch import Dispatch
-from ENUMS.servicetype import type
+from ENUMS.servicetype import ServiceType
 
 
 class TestDispatch(unittest.TestCase):
@@ -21,46 +21,71 @@ class TestDispatch(unittest.TestCase):
         return {
             'orderID': 1234,
             'customerID': 42131,
-            'serviceType': type.DRYCLEANING,
+            'serviceType': ServiceType.DRYCLEANING.value,
             'destination': "St. Edward's University",
             'timeOrderMade': '12:23:43',
             }
     
     def test_createDispatch(self):
-        vehicles = self.getVehicles()
         order = self.getOrder()
-        
+        vehicles = self.getVehicles()
+    
         filteredVehicles = list(filter(lambda x: x[1] == 'Active', vehicles))
+        print(filteredVehicles)
         vehicle = filteredVehicles[0]
-        
-        vid, status, liscensePlate, fleetId, make, model, vLon, vLat = vehicle
-        
-        dispatchDict = copy.deepcopy(order);
+    
+        # Capture vehicle tuple into its separate variables
+        vid, status, licensePlate, fleetId, make, model, vLon, vLat = vehicle
+    
+        # Seeing if the unpacking worked d:
+        print(vehicle)
+        print(vid)
+        print(status)
+        print(licensePlate)
+        print(fleetId)
+        print(make)
+        print(model)
+        print(vLon)
+        print(vLat)
+    
+        vehicleDict = {
+            'vid': vid,
+            'liscensePlate': licensePlate,
+            'make': make,
+            'model': model,
+            'curLocation': {
+                'lon': vLon,
+                'lat': vLat
+                },
+            }
+    
+        print(vehicleDict)
+        dispatchDict = copy.deepcopy(order)
         dispatchDict['vid'] = vid
-        
-        # Turn a destination dictionary into a tupled pair
-        attrToTuple = dispatchDict.pop('destination');
+    
+        attrToTuple = dispatchDict.pop('destination')
         print(attrToTuple)
-        
-        # Here we would translate human readable to geo code, but for now we'll hardcode some points
-        
+    
         attrToTuple = {
             'lon': 123.12,
             'lat': 32.1
             }
-        
+    
         dispatchDict['loc_f'] = (attrToTuple['lon'], attrToTuple['lat'])
         dispatchDict['loc_0'] = (vLon, vLat)
-        
+    
+        print(dispatchDict)
+    
         dispatch = Dispatch(**dispatchDict)
-        
+    
         print(dispatch)
         # print(type(dispatch.status))
         print(
-                dispatch.vid, dispatch.cid, dispatch.oid,
+                dispatch.sType, dispatch.vid,
+                dispatch.cid, dispatch.oid,
                 dispatch.loc_0[1], dispatch.loc_0[0],
                 dispatch.loc_f[1], dispatch.loc_f[0],
-                dispatch.timeCreated, dispatch.status, dispatch.sType
+                dispatch.timeCreated, dispatch.status
                 )
 
 
