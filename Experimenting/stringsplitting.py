8825af62308d16be3bdcf20622d21888026b1732
komoto415 <jeffreyng99@gmail.com>
2020-03-13 16:00:11 -0500

    more experiments with data structure methods

---
+++ Experimenting/stringsplitting.py
@@ -0,0 +1,20 @@
+# str = "https://supply.team22.softwareengineeringii.com/getDispatch/?vid=123&status=queued&status=queued&status=queued"
+str = "https://supply.team22.softwareengineeringii.com/getDispatch/?vid=123"
+
+str = str.split('/')[-1].strip('?')
+print(str)
+vid = str.split('=')[1]
+print(vid)
+
+assert(vid == '123')
+
+arr = str.split('&')
+print(arr)
+arr1 = [x.split('=')[0] for x in arr]
+arr2 = [x.split('=')[1] for x in arr]
+arr3 = [x.split('=') for x in arr if x.split('=')[0] == 'vid']
+print(arr1)
+print(arr2)
+print(arr3)
+dict1 = dict(zip(arr1, arr2))
+print(dict1)
