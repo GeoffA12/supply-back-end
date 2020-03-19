8f7aea6617fa122b327fd83274df1a3c9ec86532
Jeffrey Ng <komoto415@gmail.com>
2020-03-18 23:07:39 -0500

    fixed typo

--- fleet.py
+++ fleet.py
@@ -1,14 +1,14 @@
 # from utils.serverutils import connectToSQLDB
 
 class Fleet(object):
-    ver = '0.1'
+    ver = '0.1.1'
     
     def __init__(self, fleetid, fmid, region, serviceType):
         self.fleetid = fleetid
         self.fmid = fmid
         self.region = region
         self.serviceType = serviceType
-    
+
     # def getVehicles(self):
     #     print('get vehicles')
     #     sqlConnection = connectToSQLDB()
@@ -18,13 +18,13 @@ class Fleet(object):
     #         vehicleList = cursor.fetchall()
     #         sqlConnection.close()
     #         return dict(enumerate(x for x in vehicleList))
-    
-    def asddisict(self):
+
+    def asdict(self):
         return self.__dict__
-    
+
     def __repr__(self):
         return f'''{self.fleetid}, {self.fmid}, {self.region}, {self.serviceType}'''
-    
+
     def __str__(self):
         return f'''Fleet ID: {self.fleetid}
 Fleet Manager: {self.fleetid}
