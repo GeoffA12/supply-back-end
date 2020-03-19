bd0846a5a1ef35eff59d175b6405d00e5a311183
Jeffrey Ng <komoto415@gmail.com>
2020-03-18 21:46:59 -0500

    testing python tools

--- Experimenting/stringsplitting.py
+++ Experimenting/stringsplitting.py
@@ -1,4 +1,4 @@
-str = "https://supply.team22.softwareengineeringii.com/getDispatch/?vid=123&status=queued&status=queued&status=queued"
+str = "https://supply.team22.softwareengineeringii.com/getDispatch/?vid=123&test=queued&tess=queued&tesa=queued"
 # str = "https://supply.team22.softwareengineeringii.com/getDispatch/?vid=123"
 
 str = str.split('/')[-1].strip('?')
@@ -12,9 +12,9 @@ arr = str.split('&')
 print(arr)
 arr1 = [x.split('=')[0] for x in arr]
 arr2 = [x.split('=')[1] for x in arr]
-arr3 = [x.split('=') for x in arr if x.split('=')[0] == 'vid']
+# arr3 = [x.split('=') for x in arr if x.split('=')[0] == 'vid']
 print(arr1)
 print(arr2)
-print(arr3)
-dict1 = dict(zip(arr1, arr2))
-print(dict1)
+# print(arr3)
+# dict1 = dict(zip(arr1, arr2))
+# print(dict1)
