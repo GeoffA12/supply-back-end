5169baf31d16840ec7e2ac531ab3a13f296bb0aa
Jeffrey Ng <komoto415@gmail.com>
2020-03-18 21:46:40 -0500

    exception thrown for not using enums, aligned naming with DB tables

--- dispatch.py
+++ dispatch.py
@@ -1,20 +1,28 @@
 from datetime import datetime
 
 from enums.dispatchstatus import DispatchStatus
+from enums.servicetype import ServiceType
 
 
 class Dispatch(object):
     ver = '0.5'
     
-    def __init__(self, serviceType, vid, customerID, orderID, loc_0, loc_f, timeOrderMade):
-        self.sType = serviceType
+    def __init__(self, serviceType, vid, customerID, orderID, loc_0, loc_f, timeOrderMade,
+                 status = DispatchStatus.QUEUED):
+        if type(serviceType) is not ServiceType:
+            raise TypeError("Must use enum!")
+        
+        elif type(status) is not DispatchStatus:
+            raise TypeError("Must use enum!")
+        
+        self.serviceType = serviceType
         self.vid = vid
-        self.cid = customerID
-        self.oid = orderID
+        self.custid = customerID
+        self.orderid = orderID
         self.loc_0 = loc_0
         self.loc_f = loc_f
-        self.timeCreated = datetime.strptime(timeOrderMade, '%H:%M:%S').time()
-        self.status = DispatchStatus.QUEUED
+        self.timeCreated = timeOrderMade
+        self.status = status
     
     def getRoute(self, curLoc):
         print('my route')
@@ -23,22 +31,21 @@ class Dispatch(object):
     def getETA(self, curLoc):
         print('my eta')
         # do stuff to get ETA, will probably need DateTime.now stuff
-
+    
     def asdict(self):
         return self.__dict__
     
     def __repr__(self):
         return f'Dispatch(' \
-               f'{self.sType}, {self.vid}, {self.cid}, {self.oid}, ' \
+               f'{self.serviceType}, {self.vid}, {self.custid}, {self.orderid}, ' \
                f'{self.loc_0}, {self.loc_f}, {self.timeCreated}'
     
     def __str__(self):
-        return f'''Service Type: {self.sType}
+        return f'''Service Type: {self.serviceType}
 Vehicle ID: {self.vid}
-Customer ID: {self.cid}
-Order ID: {self.oid}
+Customer ID: {self.custid}
+Order ID: {self.orderid}
 Start Location: {self.loc_0}
 End Location: {self.loc_f}
 Time Order was Placed: {self.timeCreated},
-Dispatch Status: {self.status}
-'''
+Dispatch Status: {self.status}'''
