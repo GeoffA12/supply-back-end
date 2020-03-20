05e0ede2624e0c67a995f24812ef0ea6c9c84803
Jeffrey Ng <komoto415@gmail.com>
2020-03-20 00:22:28 -0500

    now all protected variables with @propery, constructor utilises asserts

--- dispatch.py
+++ dispatch.py
@@ -1,28 +1,57 @@
 from datetime import datetime
-
+from utils.serverutils import connectToSQLDB
 from enums.dispatchstatus import DispatchStatus
 from enums.servicetype import ServiceType
 
 
 class Dispatch(object):
     ver = '0.5.0'
-
+    
     def __init__(self, serviceType, vid, custid, orderid, loc_0, loc_f, timeOrderMade,
-                 status = DispatchStatus.QUEUED):
-        if type(serviceType) is not ServiceType:
-            raise TypeError("Must use enum!")
+                 status=DispatchStatus.QUEUED):
+        assert type(serviceType) is ServiceType
+        assert type(status) is DispatchStatus
+        
+        self._serviceType = serviceType
+        self._vid = vid
+        self._custid = custid
+        self._orderid = orderid
+        self._loc_0 = loc_0
+        self._loc_f = loc_f
+        self._timeCreated = timeOrderMade
+        self._status = status
     
-        elif type(status) is not DispatchStatus:
-            raise TypeError("Must use enum!")
+    @property
+    def serviceType(self):
+        return self._serviceType
     
-        self.serviceType = serviceType
-        self.vid = vid
-        self.custid = custid
-        self.orderid = orderid
-        self.loc_0 = loc_0
-        self.loc_f = loc_f
-        self.timeCreated = timeOrderMade
-        self.status = status
+    @property
+    def vid(self):
+        return self._vid
+    
+    @property
+    def custid(self):
+        return self._custid
+    
+    @property
+    def orderid(self):
+        return self._orderid
+    
+    @property
+    def loc_0(self):
+        return self._loc_0
+    
+    @property
+    def loc_f(self):
+        return self._loc_f
+    
+    @property
+    def timeCreated(self):
+        return self._timeCreated
+    
+    @property
+    def status(self):
+        return self._status
     
     def getRoute(self, curLoc):
         print('my route')
@@ -31,6 +60,17 @@ class Dispatch(object):
     def getETA(self, curLoc):
         print('my eta')
         # do stuff to get ETA, will probably need DateTime.now stuff
+    
+    def completed(self):
+        self._status = DispatchStatus.DONE
+        sqlConnection = connectToSQLDB()
+        statement = 'UPDATE dispatch SET status = 3 WHERE orderid = %s'
+        cursor = sqlConnection.cursor()
+        cursor.execute(statement, (self._orderid,))
+        cursor.commit()
+        cursor.close()
+        sqlConnection.close()
+        # Update in DB
     
     def asdict(self):
         return self.__dict__
