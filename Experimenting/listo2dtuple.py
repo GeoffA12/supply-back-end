6d135634c7e40ff27c24db758488812c7950dfe1
Jeffrey Ng <komoto415@gmail.com>
2020-04-04 02:56:54 -0500

    YEET THIS SHIT OUT

--- Experimenting/listo2dtuple.py
+++
@@ -1,6 +0,0 @@
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, ]
-tuple = tuple((x,) for x in list)
-print(tuple)
-for i in tuple:
-    print(i)
-    print(type(i))
--- Experimenting/strings.py
+++
@@ -1,9 +0,0 @@
-str = 'DRY CLEANING'
-name = str.replace(' ', '').lower()
-print(str)
-print(name)
-
-str = 'asda %s'
-data = ("asa",)
-newstr = str % data
-print(newstr)
