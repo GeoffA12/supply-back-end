7b7944ecbf87ec06b9a2b1d868c8b122f7328f0b
Jeffrey Ng <jng@stedwards.edu>
2020-04-07 10:29:58 -0500

    utilising local imports instead of global for some optimisation, notifications method added

--- utils/serverutils.py
+++ utils/serverutils.py
@@ -1,16 +1,36 @@
-import http.server
-from http.server import BaseHTTPRequestHandler
-import json
-import urllib.parse
-import mysql.connector as sqldb
-import requests
+import os
+from dotenv import load_dotenv
 
-# from dispatch import Dispatch
-# import datetime
-
-ver = '0.3.0'
+ver = '0.4.0'
 
 
 def connectToSQLDB():
-    return sqldb.connect(user = 'root', password = 'password', database = 'team22supply', port = 6022)
+    import mysql.connector as sqldb
+    password = os.getenv('DB_PASSWORD')
+    password = 'password'
+    return sqldb.connect(user='root', password=password, database='team22supply', port=6022)
 
+
+def notifications(recipients, subject, body, sender='noreply@wego.com'):
+    from sendgrid import SendGridAPIClient
+    from sendgrid.helpers.mail import Mail
+    
+    project_folder = os.path.expanduser('~/supply-backend/Experimenting')  # adjust as appropriate
+    load_dotenv(os.path.join(project_folder, '.env'))
+    
+    SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+    # SENDGRID_API_KEY = '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'
+    
+    message = Mail(
+            from_email=sender,
+            to_emails=recipients,
+            subject=subject,
+            html_content=body)
+    try:
+        sendgrid_client = SendGridAPIClient(SENDGRID_API_KEY)
+        response = sendgrid_client.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e)
