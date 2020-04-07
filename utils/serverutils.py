5c88998ceabb55b90f827aba662021e6f8931ffa
Jeffrey Ng <jng@stedwards.edu>
2020-04-07 10:37:17 -0500

    commenting out dotenv things until module installed server side

--- utils/serverutils.py
+++ utils/serverutils.py
@@ -1,12 +1,12 @@
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 ver = '0.4.0'
 
 
 def connectToSQLDB():
     import mysql.connector as sqldb
-    password = os.getenv('DB_PASSWORD')
+    # password = os.getenv('DB_PASSWORD')
     password = 'password'
     return sqldb.connect(user='root', password=password, database='team22supply', port=6022)
 
@@ -15,11 +15,11 @@ def notifications(recipients, subject, body, sender='noreply@wego.com'):
     from sendgrid import SendGridAPIClient
     from sendgrid.helpers.mail import Mail
     
-    project_folder = os.path.expanduser('~/supply-backend/Experimenting')  # adjust as appropriate
-    load_dotenv(os.path.join(project_folder, '.env'))
+    # project_folder = os.path.expanduser('~/supply-backend/Experimenting')  # adjust as appropriate
+    # load_dotenv(os.path.join(project_folder, '.env'))
     
-    SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
-    # SENDGRID_API_KEY = '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'
+    # SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+    SENDGRID_API_KEY = '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'
     
     message = Mail(
             from_email=sender,
