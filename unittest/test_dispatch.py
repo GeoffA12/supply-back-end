c8e8abd0389f319ea31f3826c08d2765f6f57c4a
Jeffrey Ng <jng@stedwards.edu>
2020-04-09 14:23:18 -0500

    changed incoming values to better reflect actual transaction

--- unittest/test_dispatch.py
+++ unittest/test_dispatch.py
@@ -10,7 +10,6 @@ from enums.dispatchstatus import DispatchStatus
 
 
 class TestDispatch(unittest.TestCase):
-    dispatch = None
     
     def getVehicles(self):
         return (
@@ -20,36 +19,38 @@ class TestDispatch(unittest.TestCase):
             (12345, 'qw3256', 'Toyota', 'V-8', 23.42, 124.31,)
             )
     
-    def getOrder(self):
+    def getDCOrder(self):
         return {
-            'serviceType': ServiceType.DRY_CLEANING,
+            'serviceType': ServiceType.DRY_CLEANING.name,
             'custid': 1234567,
             'orderid': 1234,
             'destination': {
                 'lat': 123,
                 'lon': 123
                 },
-            'timeOrderMade': datetime(2011, 11, 4, 0, 5, 23)
+            'timeOrderMade': "2020-03-29T13:34:00.000"
             }
-    
-    def test_createdispatch(self):
-        order = self.getOrder()
+
+    def test_createDCdispatch(self):
+        order = self.getDCOrder()
         vehicles = self.getVehicles()
         vehicle = vehicles[0]
-        
+        order['serviceType'] = ServiceType.translate(order['serviceType'])
         # Capture vehicle tuple into its separate variables
         vid, licensePlate, make, model, vLat, vLon = vehicle
-        
+    
         # Seeing if the unpacking worked d:
         print(vehicle)
-
+        print()
+    
         print(vid)
         print(licensePlate)
         print(make)
         print(model)
         print(vLon)
         print(vLat)
-        
+        print()
+    
         vehicleDict = {
             'vid': vid,
             'licensePlate': licensePlate,
@@ -60,35 +61,45 @@ class TestDispatch(unittest.TestCase):
                 'lon': vLon
                 },
             }
-
+    
         print(vehicleDict)
+        print()
+    
         dispatchDict = deepcopy(order)
         dispatchDict['vid'] = vid
-
+    
         # Turn a destination dictionary into a tupled pair
         destination = dispatchDict.pop('destination')
-
+    
         dispatchDict['loc_f'] = (destination['lat'], destination['lon'])
         dispatchDict['loc_0'] = (vLat, vLon)
-
+    
         # dispatchDict['status'] = DispatchStatus.RUNNING
-
+    
         print(dispatchDict)
-
+        print()
+    
         dispatch = Dispatch(**dispatchDict)
-
+    
         self.assertEqual(ServiceType.DRY_CLEANING, dispatch.serviceType)
         self.assertEqual(12345, dispatch.vid)
         self.assertEqual(1234567, dispatch.custid)
         self.assertEqual(1234, dispatch.orderid)
         self.assertEqual((23.42, 42.12), dispatch.loc_0)
         self.assertEqual((123, 123), dispatch.loc_f)
-        self.assertEqual(datetime(2011, 11, 4, 0, 5, 23), dispatch.timeCreated)
-        self.assertEqual(DispatchStatus.QUEUED, dispatch.status)
-
+        self.assertEqual("2020-03-29T13:34:00.000", dispatch.timeCreated)
+        self.assertEqual(DispatchStatus.RUNNING, dispatch.status)
+    
         print(dispatch)
-
-        self.assertEqual(1, dispatch.status.value)
+        print()
+        self.assertEqual(2, dispatch.status.value)
+    
+        dispatch.completed()
+        self.assertEqual(DispatchStatus.DONE, dispatch.status)
+        print(dispatch)
+        print()
+    
+        print(repr(dispatch))
 
 
 if __name__ == '__main__':
