fe35280d99ed66ffa3411c2a04a4b9bb11027666
Jeffrey Ng <jng@stedwards.edu>
2020-04-09 14:23:49 -0500

    added tests for the rest of the fm attributes, including try excepts for fail cases

--- unittest/test_fleetmanager.py
+++ unittest/test_fleetmanager.py
@@ -1,5 +1,6 @@
 import unittest
 import sys
+from copy import deepcopy
 
 sys.path.insert(1, '../')
 from fleetmanager import FleetManager
@@ -16,9 +17,41 @@ postBody = {
 class MyTestCase(unittest.TestCase):
     def test_createFleetManager(self):
         fm = FleetManager(**postBody)
-        print(fm)
+        
         self.assertEqual('komoto415', fm.username)
+        self.assertEqual('komoto415@gmail.com', fm.email)
+        self.assertEqual('password', fm.password)
+        self.assertEqual('Jeffrey', fm.firstname)
+        self.assertEqual('Ng', fm.lastname)
+        self.assertEqual('1234567890', fm.phonenumber)
         self.assertEqual('Yes', fm.fleetIDs)
+        
+        print(fm)
+        print()
+        
+        print(repr(fm))
+        print()
+    
+    def test_failedToCreateByEmail(self):
+        postBodyCopy = deepcopy(postBody)
+        postBodyCopy['email'] = 'komoto415.com'
+        try:
+            fm = FleetManager(**postBodyCopy)
+        except ValueError as ve:
+            print(ve)
+            print('Yay! We hit hit a ValueError')
+        print()
+    
+    def test_failedToCreateByPassword(self):
+        postBodyCopy = deepcopy(postBody)
+        postBodyCopy['password'] = '123'
+        try:
+            fm = FleetManager(**postBodyCopy)
+        except ValueError as ve:
+            print(ve)
+            print('Yay! We hit hit a ValueError')
+        print()
+
 
 if __name__ == '__main__':
     unittest.main()
