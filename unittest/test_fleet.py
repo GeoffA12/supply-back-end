21222c2fc6b731bd34eccb35f0fbf026a8634efb
Jeffrey Ng <jng@stedwards.edu>
2020-04-09 14:21:37 -0500

    adding more coverage for str nd repr methods

--- unittest/test_fleet.py
+++ unittest/test_fleet.py
@@ -9,12 +9,17 @@ from enums.servicetype import ServiceType
 class MyTestCase(unittest.TestCase):
     def test_createfleet(self):
         fleet = Fleet(123, 12, 'Austin', ServiceType.COFFEE)
-        print(fleet)
-        
+
         self.assertEqual(123, fleet.fleetid)
         self.assertEqual(12, fleet.fmid)
         self.assertEqual('Austin', fleet.region)
         self.assertEqual(ServiceType.COFFEE, fleet.serviceType)
+
+        print(fleet)
+        print()
+
+        print(repr(fleet))
+        print()
 
 
 if __name__ == '__main__':
